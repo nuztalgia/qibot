59b8e6d7a70d7f6c5d5fef7c0526d0b8fafa4c7b
Nuz / Lovegood <nuztalgia@gmail.com>
2022-07-10 13:58:14 -0700

    Move dependency specs from pyproject.toml to setup.py.
    This is purely because Renovate currently does not support `pyproject.toml` as a Python dependency manager: https://github.com/renovatebot/renovate/issues/10187

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
 from setuptools import setup
 
-setup()
+setup(
+    install_requires=[
+        "aiohttp == 3.8.1",
+        "humanize == 4.2.3",
+        "pillow == 9.2.0",
+        "py-cord == 2.0.0",
+    ],
+    extras_require={
+        "dev": [
+            "black == 22.6.0",
+            "isort == 5.10.1",
+            "pre-commit == 2.19.0",
+        ],
+    },
+)
