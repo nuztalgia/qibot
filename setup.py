edd4af488553b11b1a45fe44294af4d14c24922d
Nuz / Lovegood <nuztalgia@gmail.com>
2022-07-10 22:53:08 -0700

    Update pyproject.toml and setup.py again.
    The `authors` and `urls` fields in `pyproject.toml` didn't seem to work as expected, so use these weird workarounds for now to get everything to display properly in `pip show` and PyPI.

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup
 
 setup(
+    url="https://github.com/nuztalgia/qibot",
+    project_urls={
+        "Issue Tracker": "https://github.com/nuztalgia/qibot/issues",
+        "Source Code": "https://github.com/nuztalgia/qibot",
+    },
     install_requires=[
         "aiohttp >=3.8.0",
         "humanize >=4.2.3",
